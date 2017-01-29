5c7679af1d595bbd36fedda516bc2b955dcb2fc5
Ilya Zhukov <izhukov1992@gmail.com>
2017-01-29 20:25:50 +0300

    Use local allowed host. Use default file storage

--- personal/settings.py
+++ personal/settings.py
@@ -91,7 +91,7 @@ USE_MODELTRANSLATION = True
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['izhukov.herokuapp.com',]
+ALLOWED_HOSTS = ['192.168.1.4',]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -343,8 +343,3 @@ else:
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 COMMENTS_DISQUS_SHORTNAME = "izhukov"
-
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "izhukov"
